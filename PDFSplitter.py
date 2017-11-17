257fa6eb5e2c625075c714d1cc0754b50228df0f
Albert Lee <AlbertLee@Alberts-MacBook-Pro.local>
2017-11-17 09:24:27 -0500

    minor modification to naming of split files

--- PDFSplitter.py
+++ PDFSplitter.py
@@ -5,14 +5,7 @@ for file in os.listdir(dir_path + '/Files'):
     if file != '.DS_Store':
         inputpdf = PdfFileReader(open('Files/' + file, "rb"))
         for i in range(inputpdf.numPages):
-            if i in [0, 1, 2]:
-                y = i + 1
-            elif i == 3:
-                y = 45
-            elif i == 4:
-                y = i
-            elif i == 5:
-                y = i
+            y = i + 1
             output = PdfFileWriter()
             output.addPage(inputpdf.getPage(i))
             with open('Outputs/' + file.replace('.pdf','') + "_d%s.pdf" % str(y), "wb") as outputStream:
